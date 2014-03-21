e9dac1af4959f8b341dcad4bf2374f74889e1fec
Sander <sandermoolin@gmail.com>
2014-03-21 13:16:15 -0400

    Development of actions sub-window

--- Assets/scripts/GUI/GUI_StatsDisplay.cs
+++ Assets/scripts/GUI/GUI_StatsDisplay.cs
@@ -1,13 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GUI_StatsDisplay : MonoBehaviour {
 
 	public GUISkin skin;
+	Dictionary<string, bool> actions = new Dictionary<string, bool>() {
+		{ "Full Stomach", false },
+		{ "Awake", false },
+		{ "Fit", false },
+		{ "Mindful", false },
+		{ "Convicted", false },
+		{ "Well-Disposed", false }
+	};
+
+
+
+
+	// ACTION DISPLAY
+	// ================
+	string actionsToShow = string.Empty;
+
+	void ActionsMenu () {
+		switch (actionsToShow) {
+
+		case "Full Stomach":
+			GUILayout.Box ("test");
+			print ("yup");
+			break;
+		};
+	}
+
+
+	// GRAPH DISPLAY
+	// ================
 
 	public float spacer = 10f;
 	public float maxGraphWidth;
-
+	
 	public Texture2D fullStomachGraph;
 	public Texture2D awakeGraph;
 	public Texture2D fitGraph;
@@ -16,15 +46,13 @@ public class GUI_StatsDisplay : MonoBehaviour {
 	public Texture2D dispositionGraph;
 	public Texture2D blackGraph;
 
-
-	void ActivateStatInfo (string whichStat) {}
-
 	void DisplayStat (string name, float toDisplay, Texture2D graphBar, float scale = 100f, bool includeSpace = true) {
 
 		GUILayout.BeginHorizontal();
 		// activates options relating to that stat
 		if (GUILayout.Button (name, GUILayout.Width(100f))) {
-			ActivateStatInfo (name);
+			//ActivateStatInfo (actionsToShow);
+			actionsToShow = name;
 		}
 		// creates the appropriate box size
 		GUILayout.Box (string.Empty);
@@ -62,7 +90,16 @@ public class GUI_StatsDisplay : MonoBehaviour {
 
 		GUILayout.EndArea ();
 
+		// Left side
+		GUILayout.BeginArea (new Rect (spacer, spacer, Screen.width / 2 - spacer * 3, Screen.height - spacer * 2));
+		ActionsMenu ();
+		GUILayout.EndArea();
 
 
+
+	}
+
+	void Update () {
+		print (actionsToShow);
 	}
 }
