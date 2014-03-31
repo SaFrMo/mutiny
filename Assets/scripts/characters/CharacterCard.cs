f09dd58353791994e0e2f6b1ee76ea963c115800
Sander <sandermoolin@gmail.com>
2014-03-31 19:22:32 -0400

    Developed Character Card and example prefab

---
+++ Assets/scripts/characters/CharacterCard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterCard : MonoBehaviour {
+
+	public string characterName;
+	public int personalityType;
+	public Dictionary<string, int> relationships = new Dictionary<string, int>();
+	public Texture2D portrait;
+}
