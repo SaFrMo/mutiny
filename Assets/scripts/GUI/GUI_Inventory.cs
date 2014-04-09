eb677c3c2d9532c739bf92c7e90f6e2500fcc6a5
Sander <sandermoolin@gmail.com>
2014-04-09 09:53:10 -0400

    added awesome sliding inventory window

--- Assets/scripts/GUI/GUI_Inventory.cs
+++ Assets/scripts/GUI/GUI_Inventory.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class GUI_Inventory : MonoBehaviour {
 
+	public GUISkin skin;
+
+	private float alpha = 0;
+	private float buttonWidth = 100f;
+	private float buttonHeight = 100f;
+	private float inventoryWidth = 300f;
+	private float inventoryHeight = 500f;
+	private float currentInventoryWidth = 0;
+	private float resizeRate = 25f;
+
+	private bool showInventory = false;
+	
+
 	private void DrawInventoryButton () {
-		if (Input.mousePosition.y <= 0) {
-			// TODO: Scoot everything up X pixels?
+		Rect buttonLocation = new Rect (Screen.width - buttonWidth - currentInventoryWidth, Screen.height / 2 - buttonHeight, buttonWidth, buttonHeight);
+
+		if (GUI.Button (buttonLocation, "I\nN\nV")) {
+			showInventory = !showInventory;
 		}
 	}
 
+	private void DrawInventoryWindow () {
+		if (showInventory) {
+			// expand the window if applicable
+			if (currentInventoryWidth < inventoryWidth) {
+				currentInventoryWidth += resizeRate;
+			}
+		}
+		else {
+			// contract the window if applicable
+			if (currentInventoryWidth > 0) {
+				currentInventoryWidth -= resizeRate;
+			}
+		}
+		// draw the window itself
+		GUILayout.BeginArea (new Rect (Screen.width - currentInventoryWidth, Screen.height / 2 - buttonHeight, inventoryWidth, inventoryHeight));
+		GUILayout.Box ("test");
+		GUILayout.EndArea();
+
+	}
+
 	void OnGUI () {
+		GUI.skin = skin;
 		DrawInventoryButton();
+		DrawInventoryWindow();
 	}
 }
