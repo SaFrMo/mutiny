d79a93344689b3db88a20bc6b6ae5c86a0876a1a
Sander <sandermoolin@gmail.com>
2014-04-09 19:46:57 -0400

    More crafting system work

--- Assets/scripts/GUI/GUI_Crafting.cs
+++ Assets/scripts/GUI/GUI_Crafting.cs
@@ -29,13 +29,25 @@ public class GUI_Crafting : IGUIMenu {
 	private float cellsPerRow = 5f;
 
 	void Start () {
-		inventoryWidth = Screen.width * .45f;
+		inventoryWidth = Screen.width * .25f;
 		inventoryHeight = Screen.height * .45f;
 		cellSide = inventoryWidth / cellsPerRow;
 	}
 
 	void DrawCell (string objectName, Texture2D portrait) {
-		GUILayout.Box (objectName, GUILayout.Width(cellSide), GUILayout.Height(cellSide));
+		if (GUILayout.Button (objectName, GUILayout.Width(cellSide), GUILayout.Height(cellSide))) {
+			if (itemsToCraft.Contains (objectName)) {
+				itemsToCraft.Remove (objectName);
+			}
+			else {
+				if (itemsToCraft.Count < maxCraftingSize) {
+					itemsToCraft.Add (objectName);
+				}
+				else {
+					//TODO: "not enough crafting space" error
+				}
+			}			
+		}
 	}
 
 	void DrawInventory () {
@@ -53,10 +65,73 @@ public class GUI_Crafting : IGUIMenu {
 
 	}
 
+	void DrawCrafting () {
+		GUILayout.BeginArea (new Rect (Screen.width / 2 - inventoryWidth, Screen.height * .75f, inventoryWidth / 2, inventoryHeight / 2));
+
+		// representation of itemsToCraft list
+		string craftingList = string.Empty;
+		foreach (string s in itemsToCraft) {
+			craftingList += s + "\n";
+		}
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Box (craftingList);
+		if (GUILayout.Button ("[TRY CRAFTING]")) {
+			AttemptCrafting();
+		}
+		GUILayout.EndHorizontal();
+
+		GUILayout.EndArea();
+	}
+
+	// CRAFTING SECTION
+	// ==================
+
+	private int maxCraftingSize = 3;
+	private List<string> itemsToCraft = new List<string>();
+
+	private bool ListsMatch (List<string> a, List<string> b) {
+		// return false if they're not the same size
+		if (a.Count != b.Count) {
+			return false;
+		}
+		else {
+			foreach (string s in a) {
+				if (!b.Contains (s)) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+
+	private void AttemptCrafting () {
+		List<string> result = craftingReference.Find (x => ListsMatch (itemsToCraft, x));
+		print (result);
+		if (result != null) {
+			itemsToCraft.Clear ();
+		}
+		else {
+			// TODO: error message, no such thing
+		}
+	}
+
+	// MAIN CRAFTING LIST
+	private List<List<string>> craftingReference = new List<List<string>>() {
+		new List<string>() { "1", "2", "3" }
+	};
+
+
+	// ONGUI() SECTION
+	// ==================
+
 	void OnGUI () {
 		GUI.skin = skin;
 		if (DISPLAYED) {
+			itemsToCraft.Sort();
 			DrawInventory();
+			DrawCrafting();
 		}
 	}
 }
