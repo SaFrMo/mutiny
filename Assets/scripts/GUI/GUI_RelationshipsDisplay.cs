be265e267c1b8b4afaeafb96407ce85cbda1052a
Sander <sandermoolin@gmail.com>
2014-04-11 10:17:54 -0400

    Bugfix: prevent multiple draws on GUIRelationshipDisplay

--- Assets/scripts/GUI/GUI_RelationshipsDisplay.cs
+++ Assets/scripts/GUI/GUI_RelationshipsDisplay.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿
+
+
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Vectrosity;
 
 public class GUI_RelationshipsDisplay : IGUIMenu {
-
+	
 	// interface members
 	public override string ButtonName() {
 		return "Social View";
 	}
-
+	
 	public override void Display () {
 		DISPLAYED = true;
 	}
@@ -19,26 +23,26 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 		DISPLAYED = false;
 		DestroyAndClearVectorLineReference();
 	}
-
+	
 	// class
 	public bool DISPLAYED = false;
-
-
-
-
+	
+	
+	
+	
 	public GUISkin skin;
 	public float cellWidth = 100f;
 	public float cellHeight = 100f;
 	public float spacer = 5f;
-
+	
 	private bool ctrlIsDown;
 	private GameObject[] toShowOnActivation;
-
+	
 	// Contains a reference to each crewmember and their cell location
 	Dictionary<GameObject, Vector3> crewmemberCellLocation = new Dictionary<GameObject, Vector3>();
 	Dictionary<GameObject, Vector2[]> crewmemberLinesReference = new Dictionary<GameObject, Vector2[]>();
 	Dictionary<GameObject, VectorLine> crewmemberVectorLineReference = new Dictionary<GameObject, VectorLine>();
-
+	
 	void DestroyAndClearVectorLineReference (GameObject exceptThisOne = null) {
 		foreach (KeyValuePair<GameObject, VectorLine> kv in crewmemberVectorLineReference) {
 			if (kv.Key != exceptThisOne) {
@@ -53,7 +57,7 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 	void CharacterCell (Rect groupPosition, GameObject character) {
 		GUI.BeginGroup (groupPosition);
 		if (GUI.Button (new Rect (0, 0, cellWidth, cellHeight), string.Format ("{0}\n{1}", character.name, character.GetComponent<CharacterCard>().yourRelationship))) {
-
+			
 			// "Control" key to manipulate single item in list of selected GOs
 			if (crewmemberVectorLineReference.ContainsKey (character)) {
 				if (!ctrlIsDown && crewmemberVectorLineReference.Count > 1) {
@@ -79,28 +83,33 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 		}
 		GUI.EndGroup();
 	}
-
-
-
+	
+	
+	
 	void DrawRelationshipCircle () {
 		for (int i = 0; i < GAME_MANAGER.Roster.Count; i++) {
 			Vector2 cellLocation = new Vector2 (crewmemberCellLocation[GAME_MANAGER.Roster[i]].x, crewmemberCellLocation[GAME_MANAGER.Roster[i]].y);
 			CharacterCell (new Rect (cellLocation.x - cellWidth / 2, cellLocation.y - cellHeight / 2, cellWidth, cellHeight), GAME_MANAGER.Roster[i]);
 		}
 	}
-
+	
 	private float relationshipNumbersSize = 50f;
-
+	
 	void DrawRelationshipNumbers () {
-		// TODO: prevent multiple draws
+		// TODO: prevent multiple draws - RESOLVED?
 		foreach (GameObject source in crewmemberVectorLineReference.Keys) {
 			foreach (GameObject other in GAME_MANAGER.Roster) {
 				if (source != other) {
 					Vector3 sourceLocation = crewmemberCellLocation[source];
 					Vector3 otherLocation = crewmemberCellLocation[other];
 					Vector3 average = (sourceLocation + otherLocation) / 2;
+					// prevents multiple draws - places relationship cell in the middle if both are selected, otherwise leans toward source
+					if (!crewmemberVectorLineReference.ContainsKey (other)) {
+						// leans toward the source to prevent drawing over each other
+						average = Vector3.MoveTowards (average, sourceLocation, crewmemberVectorLineReference[source].GetLength() * .0625f);
+					}
 					float averageFeeling = ((source.GetComponent<CharacterCard>().relationships[other] + 
-					                        other.GetComponent<CharacterCard>().relationships[source]) / 2);
+					                         other.GetComponent<CharacterCard>().relationships[source]) / 2);
 					// TODO: custom skin
 					GUI.Box (new Rect (average.x - relationshipNumbersSize / 2, average.y - relationshipNumbersSize / 2, relationshipNumbersSize,
 					                   relationshipNumbersSize), averageFeeling.ToString ());
@@ -109,7 +118,7 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 		}
 	}
 	
-
+	
 	void Start () {
 		// first, grab the list of point Vector3s for the circle
 		List<Vector3> points = SaFrMo.DrawCirclePoints (GAME_MANAGER.Roster.Count, 200f, Screen.width / 2, Screen.height / 2);
@@ -120,8 +129,8 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 		}
 		
 		// then, draw the cell in the appropriate location
-
-
+		
+		
 		// create a VectorLine for each character
 		foreach (GameObject source in GAME_MANAGER.Roster) {
 			List<Vector2> pointsList = new List<Vector2>();
@@ -139,12 +148,12 @@ public class GUI_RelationshipsDisplay : IGUIMenu {
 			crewmemberLinesReference.Add (source, pointsList.ToArray());
 		}
 	}
-
+	
 	void Update () {
 		ctrlIsDown = Input.GetKey (KeyCode.LeftControl);
 	}
-
-
+	
+	
 	void OnGUI () {
 		if (DISPLAYED) {
 			GUI.skin = skin;
