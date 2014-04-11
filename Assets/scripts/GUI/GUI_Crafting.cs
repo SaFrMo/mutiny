1f2bb2dcc47c570bbfb576852a80884b9d9eb2d8
Sander <sandermoolin@gmail.com>
2014-04-11 10:19:35 -0400

    Added crafting result message

--- Assets/scripts/GUI/GUI_Crafting.cs
+++ Assets/scripts/GUI/GUI_Crafting.cs
@@ -3,37 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GUI_Crafting : IGUIMenu {
-
+	
 	// inherited members
 	private bool DISPLAYED = false;
-
+	
 	public override string ButtonName () {
 		return "Item Creation";
 	}
-
+	
 	public override void Hide () {
 		DISPLAYED = false;
 	}
-
+	
 	public override void Display () {
 		DISPLAYED = true;
 	}
-
+	
 	// class members
 	public GUISkin skin;
-
+	
 	private float inventoryWidth;
 	private float inventoryHeight;
-
+	
 	private float cellSide;
 	private float cellsPerRow = 5f;
-
+	
 	void Start () {
 		inventoryWidth = Screen.width * .25f;
 		inventoryHeight = Screen.height * .45f;
 		cellSide = inventoryWidth / cellsPerRow;
 	}
-
+	
 	void DrawCell (Ingredient ingredient, Texture2D portrait) {
 		if (GUILayout.Button (ingredient.Name, GUILayout.Width(cellSide), GUILayout.Height(cellSide))) {
 			if (itemsToCraft.Contains (ingredient)) {
@@ -49,7 +49,7 @@ public class GUI_Crafting : IGUIMenu {
 			}			
 		}
 	}
-
+	
 	void DrawInventory () {
 		GUILayout.BeginArea (new Rect (Screen.width / 2 - inventoryWidth, Screen.height * .25f, inventoryWidth, inventoryHeight));
 		if (Player.INVENTORY.Count == 0) { Player.INVENTORY.Add (CRAFTING_MASTER.nothing); }
@@ -63,18 +63,20 @@ public class GUI_Crafting : IGUIMenu {
 		// wrap up the last row
 		GUILayout.EndHorizontal();
 		GUILayout.EndArea();
-
+		
 	}
-
+	
+	private string resultMessage = string.Empty;
+	
 	void DrawCrafting () {
 		GUILayout.BeginArea (new Rect (Screen.width / 2 - inventoryWidth, Screen.height * .75f, inventoryWidth / 2, inventoryHeight / 2));
-
+		
 		// representation of itemsToCraft list
 		string craftingList = string.Empty;
 		foreach (Ingredient s in itemsToCraft) {
 			craftingList += s.Name + "\n";
 		}
-
+		
 		GUILayout.BeginHorizontal();
 		GUILayout.Box (craftingList);
 		if (GUILayout.Button ("[TRY CRAFTING]")) {
@@ -89,60 +91,38 @@ public class GUI_Crafting : IGUIMenu {
 					}
 					catch { Debug.Log ("Couldn't delete item from inventory: " + i.Name); }
 				}
+				resultMessage = string.Format ("Created {0}!", newProduct.Name);
 				Player.INVENTORY.Add (newProduct);
 				itemsToCraft.Clear ();
 			}
-			// TODO: Crafting failure
-
+			else {
+				resultMessage = "Couldn't make anything from these!";   
+			}
+			
 		}
+		// box containing result/error message
+		GUILayout.Box (resultMessage);
 		GUILayout.EndHorizontal();
-
+		
 		GUILayout.EndArea();
 	}
-
+	
 	// CRAFTING SECTION
 	// ==================
-
+	
 	private int maxCraftingSize = 3;
 	private List<Ingredient> itemsToCraft = new List<Ingredient>();
-
-
-
-
-
-	/*
-	private void AttemptCrafting () {
-		//int resultIndex = craftingReference.FindIndex (x => ListsMatch (itemsToCraft, x));
-		if (resultIndex != -1) {
-			foreach (string x in itemsToCraft) {
-				Player.INVENTORY.Remove (x);
-			}
-			if (Player.INVENTORY.Count == 0) {
-				Player.INVENTORY.Add ("Inventory empty!");
-			}
-			itemsToCraft.Clear ();
-			// TODO: add crafted item
-		}
-		else {
-			// TODO: error message, no such thing
-		}
-	}
-	*/
-
-	// MAIN CRAFTING LIST
-	// TODO: clean this up. this is very sloppy.
-
-
-
+	
+	
 	// ONGUI() SECTION
 	// ==================
-
+	
 	void OnGUI () {
 		GUI.skin = skin;
 		if (DISPLAYED) {
-			//itemsToCraft.Sort();
 			DrawInventory();
 			DrawCrafting();
 		}
 	}
 }
+
